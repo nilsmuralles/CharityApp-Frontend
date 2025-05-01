c9af061044eec1add892e259b4d663f97f5601dc
Diego Flores <floresdiego041@gmail.com>
2025-05-01 16:46:23 -0600

    feat: add ColumnChart component and ChartsView view

---
+++ src/components/ColumnChart.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = withDefaults(defineProps<{
+  series: Array<any>
+  categories: string[]
+  title: string
+  height?: number
+  width?: number
+}>(), {
+  height: 400,
+  width: 600
+})
+
+const computedOptions = computed(() => ({
+  chart: { 
+    id: 'basic-bar',
+    animations: {
+      enabled: true,
+      speed: 100,
+      animateGradually: {
+        enabled: true,
+        delay: 150
+      },
+      dynamicAnimation: {
+        enabled: true,
+        speed: 350
+      }
+    },
+  },
+  title: {
+    text: props.title,
+    align: 'center',
+    offsetX: 0,
+    offsetY: 12,
+    style: {
+      fontSize:  '1.25rem',
+      fontWeight:  'bold',
+    },
+  },
+  xaxis: { categories: props.categories },
+  theme: {
+    mode: 'dark', 
+    palette: 'palette3', 
+  },
+  plotOptions: {
+    bar: {
+      distributed: true,
+    }
+  },
+  legend: {
+    show: false
+  }
+}))
+</script>
+
+<template>
+  <div class="chart-container">
+    <apexchart
+    type="bar"
+    :options="computedOptions"
+    :series="series"
+    :height="props.height"
+    :width="props.width"
+    />
+  </div>
+</template>
+
+<style scoped>
+.chart-container {
+  margin: 1rem;
+  padding: 2rem 1.2rem 1.2rem 1.2rem;
+  background: #343a3f;
+  border-radius: 1rem;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+}
+</style>
+  
+
+  
---
+++ src/views/ChartsView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import ColumnChart from '@/components/ColumnChart.vue'
+
+const series = ref([
+  {
+    name: 'Donaciones',
+    data: [500,205,230,580,458,954,120,456,453,245,654,123]
+  }
+])
+const categories = ref(['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'])
+
+</script>
+
+
+<template>
+  <main>
+    
+    <div class="container">
+      <h1>Prueba de graficas en Vue </h1>
+      <div class="chart-grid">
+        <ColumnChart :series="series" :categories="categories" title="Donaciones por Mes"  :height="300" :width="500" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container{
+  width: 100dvw;
+  height: calc(100dvh - 60px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.chart-grid{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+}
+</style>
